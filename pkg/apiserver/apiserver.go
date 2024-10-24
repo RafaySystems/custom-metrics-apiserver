aa0a1dd3a5f5da68d114a8e0884181b08cc98656
Sashidhar Reddy Polu <sashidhar@rafay.co>
2024-10-24 20:26:04 +0530

    Updated Effective version

--- pkg/apiserver/apiserver.go
+++ pkg/apiserver/apiserver.go
@@ -23,6 +23,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/serializer"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	genericapiserver "k8s.io/apiserver/pkg/server"
+	utilversion "k8s.io/apiserver/pkg/util/version"
 	"k8s.io/client-go/informers"
 	cminstall "k8s.io/metrics/pkg/apis/custom_metrics/install"
 	eminstall "k8s.io/metrics/pkg/apis/external_metrics/install"
@@ -75,7 +76,8 @@ type CompletedConfig struct {
 
 // Complete fills in any fields not set that are required to have valid data. It's mutating the receiver.
 func (c *Config) Complete(informers informers.SharedInformerFactory) CompletedConfig {
-	/*c.GenericConfig.EffectiveVersion = &utilversion.EffectiveVersion{
+	c.GenericConfig.EffectiveVersion = utilversion.NewEffectiveVersion("1.0")
+	/*EffectiveVersion{
 		Major: "1",
 		Minor: "0",
 	}*/
